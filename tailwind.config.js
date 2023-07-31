fcad0206cf3fdde1bed01d5350db51d01ea2bb02
F1reMonster <62647213+F1reMonster@users.noreply.github.com>
2023-08-01 01:47:17 +0300

    maximal remove [] in html classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,39 +26,44 @@ module.exports = {
 		},
 		extend: {
 			screens: {
-				mxxl: { max: "77.4375em" }, // 1239
-				mxxlg: { max: "68.75em" }, // 1100
-				mxlg: { max: "63.9375em" }, // 1023
-				mlg: { max: "61.9375em" }, // 991
-				mmd: { max: "47.9375em" }, // 767
-				mxmd: { max: "35.9375em" }, // 575
-				msm: { max: "30em" }, // 480
-				mlw: { max: "23.375em" }, // 374
-				lw: "30em",
-				low: "23.375em",
+				// mxxl: { max: "77.4375em" }, // 1239
+				// mxxlg: { max: "68.75em" }, // 1100
+				// mxlg: { max: "63.9375em" }, // 1023
+				// mlg: { max: "61.9375em" }, // 991
+				// mmd: { max: "47.9375em" }, // 767
+				// mxmd: { max: "35.9375em" }, // 575
+				// msm: { max: "30em" }, // 480
+				// mlw: { max: "23.375em" }, // 374
+				lw: "30em", // 480
+				low: "23.4375em", // 375
 			},
 			fontFamily: {
 				montserrat: "Montserrat, sans-serif",
 				inter: "Inter, sans-serif",
 			},
+			fontSize: {
+				ms: ["0.625rem", "120%"], // 10px
+				"4ms": ["2.5rem", "120%"], // 40px
+
+			},
 			colors: {
 				"wild-sand": "#F5F5F5", // body bg
-				"lavender-purple": "#8B7AA8", // purple
-				"tall-poppy": "#B73D25", // red
-				"deep-blush": "#E358A6", // pink
-				shark: "#212529", // some text color
-				"dusty-gray": "#999494",
-				alto: "#D9D9D9", // slider bullets
-				alabaster: "#FBFBFB",
-				ebb: "#efe7e7",
-				goldenrod: "#FCCB71", // rating star
-				tundora: "#424242",
-				"royal-blue": " #5477E7",
-				"dove-gray": "#6A6969",
-				pomegranate: "#F03B42",
-				"waikawa-gray": "#5C78AC",
-				limeade: "#679B00",
-				"persian-red": "#CA392D",
+				"lavender-purple": "#8B7AA8", // відтінок пурпурового → buttons
+				"tall-poppy": "#B73D25", // відтінок червого
+				"deep-blush": "#E358A6", // відтінок розового
+				"shark": "#212529", // відтінок сірого
+				"dusty-gray": "#999494", // відтінок сірого
+				"alto": "#D9D9D9", // slider bullets
+				"alabaster": "#FBFBFB", // відтінок сірого
+				"ebb": "#efe7e7", // відтінок сірого
+				"goldenrod": "#FCCB71", // rating star помаранчевий
+				"tundora": "#424242", // відтінок сірого → text
+				"royal-blue": " #5477E7",// відтінок винього → links
+				"dove-gray": "#6A6969", // світло сірий → text, borders
+				"pomegranate": "#F03B42", // відтінок червоного → text, borders
+				"waikawa-gray": "#5C78AC", // відтінок синього → text
+				"limeade": "#679B00", // салатовий → text
+				"persian-red": "#CA392D", // відтінок червого
 				
 				
 
@@ -97,11 +102,11 @@ module.exports = {
 				"prod-contrast-10": "#000000",
 			},
 			boxShadow: {
-				custom: "0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.25)",
-				card: "0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.25), 0 0 0 0.0625rem #999494",
+				"custom": "0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.25)",
+				"card": "0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.25), 0 0 0 0.0625rem #999494",
 			},
 			dropShadow: {
-				text: "box-shadow: 0px 0.25rem 0.25rem 0px rgba(0, 0, 0, 0.25)",
+				"text": "box-shadow: 0px 0.25rem 0.25rem 0px rgba(0, 0, 0, 0.25)",
 			},
 			backgroundImage: {
 				send: 'url("@/img/send.svg")',
@@ -111,7 +116,65 @@ module.exports = {
 				banner: "linear-gradient(90.77deg, #FFE6E6 1.76%, #F7CCC8 92.9%)",
 			},
 			borderRadius: {
-				lge: "0.625rem",
+				"lge": "0.625rem", // 10px
+				"2lge": "1.25rem", // 20px
+				"6md": "2.25rem", // 36px
+			},
+			"margin": {
+				"4.5": "1.125rem", // 18px
+				"5.5": "1.375rem", // 22px
+				"6.5": "1.625rem", // 26px
+				"7.5": "1.875rem", // 30px
+				"8.5": "2.125rem", // 34px
+				"9.5": "2.375rem", // 38px
+			},
+			padding: {
+				"4.5": "1.125rem", // 18px
+				"5.5": "1.375rem", // 22px
+				"6.5": "1.625rem", // 26px
+				"7.5": "1.875rem", // 30px
+				"8.5": "2.125rem", // 34px
+				"9.5": "2.375rem", // 38px
+				"12.5": "3.125rem", // 50px
+			},
+			height: {
+				"4.5": "1.125rem", // 18px
+				"5.5": "1.375rem", // 22px
+				"6.5": "1.625rem", // 26px
+				"7.5": "1.875rem", // 30px
+				"8.5": "2.125rem", // 34px
+				"9.5": "2.375rem", // 38px
+				"12.5": "3.125rem", // 50px
+				"13": "3.25rem", // 52px
+				"15": "3.75rem", // 60px
+				"35": "8.75rem", // 140px
+			},
+			width: {
+				"4.5": "1.125rem", // 18px
+				"5.5": "1.375rem", // 22px
+				"6.5": "1.625rem", // 26px
+				"7.5": "1.875rem", // 30px
+				"8.5": "2.125rem", // 34px
+				"9.5": "2.375rem", // 38px
+				"12.5": "3.125rem", // 50px
+				"13": "3.25rem", // 52px
+				"15": "3.75rem", // 60px
+			},
+			gap: {
+				"4.5": "1.125rem", // 18px
+				"5.5": "1.375rem", // 22px
+				"6.5": "1.625rem", // 26px
+				"7.5": "1.875rem", // 30px
+				"8.5": "2.125rem", // 34px
+				"9.5": "2.375rem", // 38px
+			},
+			spacing: {
+				"4.5": "1.125rem", // 18px
+				"5.5": "1.375rem", // 22px
+				"6.5": "1.625rem", // 26px
+				"7.5": "1.875rem", // 30px
+				"8.5": "2.125rem", // 34px
+				"9.5": "2.375rem", // 38px
 			}
 		},
 	},
